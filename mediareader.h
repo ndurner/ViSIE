6b62f7774a9b8a34c5b0c5d4c6a70203bd1b0d1c
Nils Durner <n.durner@namirial.com>
2022-03-06 17:59:30 +0100

    GoPro meta data

--- mediareader.h
+++ mediareader.h
@@ -16,6 +16,7 @@ class MediaReader
 public:
     using MetadataKV = std::tuple<std::string, std::string>;
     using Metadata = std::list<MetadataKV>;
+    using TrackSample = struct {uint64_t offset; uint64_t size; uint32_t duration;};
 
     MediaReader(AVIOContext *ctx, Exiv2::ExifData *exifData, int targetTrackID, double timeStamp);
     void extract();
@@ -32,6 +33,8 @@ private:
     uint16_t colorPrimaries;
     uint16_t colorTransfer;
     uint16_t colorMatrix;
+    bool readingGoProMeta;
+    std::vector<TrackSample> metaTrackSamples;
 
     static QString fourCCStr(int fourCC);
     void decend(AVIOContext *ctx, int64_t rangeEnd);
@@ -40,6 +43,11 @@ private:
     void handle_stsd(AVIOContext *ctx, int64_t rangeBase, int64_t rangeEnd);
     void handle_avc1(AVIOContext *ctx, int64_t atomSize);
     void handle_colr(AVIOContext *ctx, int64_t rangeEnd);
+    void handle_hdlr(AVIOContext *ctx, int64_t rangeBase, int64_t rangeEnd);
+    void handle_stco(AVIOContext *ctx, int64_t rangeBase, int64_t rangeEnd);
+    void handle_stsz(AVIOContext *ctx, int64_t rangeBase, int64_t rangeEnd);
+    void handle_mdia(AVIOContext *ctx, int64_t rangeBase, int64_t rangeEnd);
+    void handle_stts(AVIOContext *ctx, int64_t rangeBase, int64_t rangeEnd);
 };
 
 #endif // MEDIAREADER_H
